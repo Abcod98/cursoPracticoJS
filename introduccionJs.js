26b7b6580a68bc502bfd4802e866014b35e5bd67
Abraham Ubuntu <abcod98@gmail.com>
2022-08-30 17:28:39 -0600

    Variables en JS

---
+++ introduccionJs.js
@@ -0,0 +1,19 @@
+// Declarando variables
+let nombre = "Abraham";
+let apellido = "Tun Sanchez";
+let usuario = "abcod";
+let edad = "23";
+let email = "abcod98@gmial.com";
+let esMayorDeEdad = true;
+let dineroAhorrado = 1000;
+let deudas = 100;
+
+// Sumado variables (Dinero real)
+let dineroReal = dineroAhorrado - deudas;
+
+// Concatenando strings (Nombre Completo)
+let nombreCompleto = nombre + " " + apellido;
+
+// Imprimiendo valores
+console.log ("El dinero real es: " + dineroReal);
+console.log ("El nombre completo es: " + nombreCompleto);
