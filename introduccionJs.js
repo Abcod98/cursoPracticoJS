b6830cdc8565f0fbb2cb930dffd42a5b2789acd1
Abraham Ubuntu <abcod98@gmail.com>
2022-08-31 13:44:40 -0600

    Reduccion de condicionales

--- introduccionJs.js
+++ introduccionJs.js
@@ -123,3 +123,41 @@ function imprimirElementoPorElementoObjeto (obj) {
         console.log(arr[i]);
     }
 }
+
+// Reduccion de condicionales
+// Utilizando solo if
+function mensajeSuscripcion (suscripcion) {
+    if (suscripcion == 'Free') {
+        console.log("Solo puedes tomar los cursos gratis");
+        return;
+    }
+    if (suscripcion == 'Basic') {
+        console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+        return;
+    }
+    if (suscripcion == 'Expert') {
+        console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+        return;
+    }
+    if (suscripcion == "ExpertDuo") {
+        console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+        return;
+    }
+    console.warn("Este tipo de suscripcion no existe");
+}
+
+// Utilizando arrays o objetos y una sola condicional
+const objSuscripciones = {
+    free: "Solo puedes tomar los cursos gratis",
+    basic: "Puedes tomar casi todos los cursos de Platzi durante un mes",
+    expert: "Puedes tomar casi todos los cursos de Platzi durante un año",
+    expertduo: "Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año"
+}
+
+function suscripcionObj(suscripcion) {
+    if (objSuscripciones[suscripcion]) {
+        console.log(objSuscripciones[suscripcion]);
+        return;
+    }
+    console.warn("Este tipo de suscripcion no existe")
+}
