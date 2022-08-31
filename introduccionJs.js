1dc3fa075615af3b5eccb8b8439498576adbc0de
Abraham Ubuntu <abcod98@gmail.com>
2022-08-31 11:29:31 -0600

    Condicionales

--- introduccionJs.js
+++ introduccionJs.js
@@ -28,3 +28,33 @@ function saludo (name, lastname, nickname) {
 
 console.log("Mi nombre es " + completeName + ", pero prefiero que me digas " + nickname + ".");
 }
+
+// Condicionales
+// Swithc
+const tipoDeSuscripcion = "Basic";
+
+switch (tipoDeSuscripcion) {
+    case "Free":
+        console.log("Solo puedes tomar los cursos gratis");
+        break;
+    case "Basico":
+        console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+        break;
+    case "Expert":
+        console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+        break;
+    case "ExpertDuo":
+        console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+        break;
+}
+
+// If
+if (tipoDeSuscripcion == "Free") {
+    console.log("Solo puedes tomar los cursos gratis");
+} else if (tipoDeSuscripcion == "Basic") {
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+} else if (tipoDeSuscripcion == "Expert") {
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+} else {
+    console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año"); 
+}
