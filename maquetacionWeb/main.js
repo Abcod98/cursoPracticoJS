a6da9247d70b9311635517d031c0cb632ceeeb80
Abraham Ubuntu <abcod98@gmail.com>
2022-09-12 12:46:25 -0600

    Integracion del carrito de compras

--- maquetacionWeb/main.js
+++ maquetacionWeb/main.js
@@ -1,16 +1,27 @@
 const menuEmail = document.querySelector('.navbar-email')
-const desktopMenu = document.querySelector('.desktop-menu')
 const menuListIcon = document.querySelector('.menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const carritoAside = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuListIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+    carritoAside.classList.add('inactive')
 }
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive')
+    carritoAside.classList.add('inactive')
+}
+
+function toggleCarritoAside() {
+    carritoAside.classList.toggle('inactive')
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
 }
