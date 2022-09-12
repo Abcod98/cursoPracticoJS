8999746ea23691620cd8ace0d8e99bc6f8691a59
Abraham Ubuntu <abcod98@gmail.com>
2022-09-12 16:59:45 -0600

    Detalles de un producto

--- maquetacionWeb/main.js
+++ maquetacionWeb/main.js
@@ -3,7 +3,7 @@ const menuListIcon = document.querySelector('.menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
-const carritoAside = document.querySelector('.product-detail')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -13,16 +13,16 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
-    carritoAside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
 }
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive')
-    carritoAside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
 }
 
 function toggleCarritoAside() {
-    carritoAside.classList.toggle('inactive')
+    shoppingCartContainer.classList.toggle('inactive')
     mobileMenu.classList.add('inactive')
     desktopMenu.classList.add('inactive')
 }
